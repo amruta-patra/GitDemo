5ab6482f814dfadfa3e784ffdca9aaf1ac0a7ca6
Amruta <amrutapatra@gmail.com>
2020-02-21 20:25:20 +0530

    first commit

---
+++ src/test/java/Qaclickacademy/RestAPITest.java
@@ -0,0 +1,17 @@
+package Qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	@Test
+	public void deletedTwitter()
+	{
+		System.out.println("deletedTwitter");
+	}
+}
